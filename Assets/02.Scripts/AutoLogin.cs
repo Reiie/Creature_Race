3d39e9c5e59a7fa29916cf31e2b36bfdb630bb41
Kang <ysh5022@naver.com>
2016-08-03 17:06:36 +0900

    16.08.03_17:06 SimpleLogin, RacingLogic, ADS Plus

---
+++ Assets/02.Scripts/AutoLogin.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Copyright (C) 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SocialPlatforms;
+
+public class AutoLogin : MonoBehaviour
+{
+    private bool bWatingForAuth = false;
+    //private string mStatusText = "Ready.";
+
+    //Rect labelRect = new Rect(20, 20, Screen.width, Screen.height * 0.25f);
+    //Rect imageRect = new Rect(Screen.width / 2f - 200f, Screen.height / 2f - 200f, 400f, 400f);
+
+
+    void Start()
+    {
+        // Select the Google Play Games platform as our social platform implementation
+        GooglePlayGames.PlayGamesPlatform.Activate();
+
+        doLogin();
+    }
+
+    void doLogin()
+    {
+        if (bWatingForAuth)
+        {
+            return;
+        }
+
+        if (!Social.localUser.authenticated)
+        {
+            //mStatusText = "Authenticating";
+            bWatingForAuth = true;
+            Social.localUser.Authenticate((bool success) =>
+            {
+                bWatingForAuth = false;
+                if (success)
+                {
+                    //mStatusText = "Welcome " + Social.localUser.userName;
+                    string token = GooglePlayGames.PlayGamesPlatform.Instance.GetToken();
+                    Debug.Log(token);
+                }
+                else
+                {
+                    //mStatusText = "Authentication failed";
+                }
+            });
+        }
+    }
+
+    //void OnGUI()
+    //{
+    //    GUI.skin.label.fontSize = (int)(0.05 * Screen.height);
+    //    GUI.Label(labelRect, mStatusText);
+
+    //    if (Social.localUser.authenticated)
+    //    {
+    //        if (Social.localUser.image != null)
+    //        {
+    //            GUI.DrawTexture(imageRect, Social.localUser.image, ScaleMode.ScaleToFit);
+    //        }
+    //    }
+    //}
+}
---
+++ Assets/02.Scripts/csAds.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+#if UNITY_ADS
+using UnityEngine.Advertisements;
+#endif
+
+public class csAds : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    public void ShowDefaultAd()
+    {
+#if UNITY_ADS
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show();
+        }
+#endif
+    }
+
+    public void ShowRewardedAd()
+    {
+        if (Advertisement.IsReady())
+        {
+            ShowOptions options = new ShowOptions();
+            options.resultCallback = HandleShowResult;
+            Advertisement.Show(null, options);
+        }
+    }
+
+    private void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+
+                break;
+
+            case ShowResult.Skipped:
+
+                break;
+
+            case ShowResult.Failed:
+
+                break;
+        }
+    }
+}
