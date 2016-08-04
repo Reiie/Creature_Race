75f0a6ffe46f14d52a4671949e85b7244211d065
Kang <ysh5022@naver.com>
2016-08-04 12:27:40 +0900

    풀을 위한 커밋

---
+++ Assets/02.Scripts/csShowLeaderBorad.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.UI;
+using GooglePlayGames;
+
+public class csShowLeaderBorad : MonoBehaviour
+{
+    void Start()
+    {
+        GooglePlayGames.PlayGamesPlatform.Activate();
+    }
+
+    public void ShowLeaderBorad()
+    {
+        Social.ShowLeaderboardUI();
+    }
+}
